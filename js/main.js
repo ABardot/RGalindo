f652f741c3dd8d66fac310e112bbf3d6a89cb51e
ABardot <aabardot5@gmail.com>
2018-10-12 23:02:12 -0400

    Added the JS needed for the menu but need some debuging.

--- js/main.js
+++ js/main.js
@@ -1,26 +1,33 @@
-// Select DOM Items
+// Selected DOM items
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
+// This selects all items 
 const navItems = document.querySelectorAll('.nav-item');
 
-//Set Initial State Menu
-let showmenu = false;
+// Set Initial State of Menu
+let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
-    if(!showMenu) {
+    if (!showMenu) {
         menuBtn.classList.add('close');
         menu.classList.add('show');
         menuNav.classList.add('show');
         menuBranding.classList.add('show');
+        navItems.forEach(item => item.classList.add('show'));
+
+        // Set Menu
+        showMenu = true;
+    }
+    else {
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
         navItems.forEach(item => item.classList.remove('show'));
-
-        //Show Menu
         showMenu = false;
-    } else {
-
     }
 }
